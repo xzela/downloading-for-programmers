6d6df67be1e6e96be0a1ce057ebfff421756e2b8
Zeph LaFassett <zelafassett@gmail.com>
2016-10-12 10:58:50 -0700

    removing requests dependencies

--- down.py
+++ down.py
@@ -1,17 +1,16 @@
 from lxml import html
-import urllib
-import requests
+import urllib2
 
 def get_page(url):
-	page = requests.get(url)
-	tree = html.fromstring(page.content)
+	page = urllib2.urlopen(url)
+	tree = html.fromstring(page.read())
 	music_xpath = '/html/body/div[2]/div/a[1]'
 	file_a = tree.xpath(music_xpath)
 	return [file_a[0].text, file_a[0].attrib['href']]
 
 url = 'http://musicforprogramming.net/' 
-page = requests.get(url)
-tree = html.fromstring(page.content)
+page = urllib2.urlopen(url)
+tree = html.fromstring(page.read())
 links = tree.xpath('/html/body/div[3]/div[1]/div/a');
 for a in links:
 	nurl = url + a.attrib['href']
